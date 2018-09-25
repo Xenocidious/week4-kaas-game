fd3a37bef401e9a2236f2e5318d0313f360134e1
Xeno <rblxnub@gmail.com>
2018-09-25 09:53:04 +0200

    Added kaas and game

---
+++ game.js
@@ -0,0 +1,43 @@
+alert('Jij en je bondgenoot Trafalgar komen 2 vijanden tegen, Doffy en Trebol. (Alles is hoofdlettergevoelig)')
+    var begin = prompt('Val jij of valt Trafalgar Doffy aan?', '"ik" of "Traf"')
+    if (begin == 'ik'){
+	alert('Doffy ontweek je aanval, Trebol heeft ondertussen Trafalgar vermoord.')}
+	    else if (begin == 'Traf'){
+	alert('Doffy vermoord Trafalgar en jij vermoord terwijl Trebol.')}
+	                                                                                if (begin == 'ik') var wie = prompt('Val je Doffy of Trebol aan?', '"Doffy" of "Trebol"')
+	                                                                                if (wie == 'Doffy'){
+	                                                                               	alert('Trebol vermoord je. (Game Over)')}
+	                                                                                else if (wie == 'Trebol'){
+		                                                                            alert('Je vermoord Trebol')}
+		                                                                                if (begin == 'Traf') var wat = prompt('Val je Doffy aan of ga je naar Gear 2? (Gears zijn powerups)', '"aanval" of "gear2"')
+		                                                                                var wat = prompt('Val je Doffy aan of ga je naar Gear 2? (Gears zijn powerups)', '"aanval" of "gear2"')
+		                                                                                if (wat == 'aanval'){
+		                                                                               	alert('Doffy blokkeert je aanval en vermoord je. (Game Over)')}
+		                                                                                else if (wat == 'gear2'){
+		                                                                               	alert('Je word sterker.')}
+		                                                                                    var wat2 = prompt('Val je Doffy aan of ga je naar Gear 3?', '"Doffy" of "gear3"')
+		                                                                                    if (wat2 == 'Doffy'){
+		                                                                                    alert('Doffy ontwijkt je aanval en vermoord je. (Game Over)	')}
+		                                                                                    else if (wat2 == "gear3"){
+		                                                                                    alert('Je word weer sterker.')}
+		                                                                                       	var wat3 = prompt('Val je Doffy aan of ga je naar Gear 4?', '"Doffy" of "gear4"')
+		                                                                                       	if (wat3 == 'Doffy'){
+		                                                                                       	alert('Doffy blokkeert je aanval en vermoord je. (Game Over)')}
+		                                                                                        else if (wat3 == 'gear4'){
+		                                                                                        alert('Je word nog sterker')}
+		                                                                                		    var wat4 = prompt('Val je Doffy aan of ga je naar Gear 5?', '"Doffy" of "gear5"')
+				    	                                                                            if (wat4 == 'gear5'){
+				    	                                                                           	alert('Je ontploft. (Game Over)')}
+				    	                                                                            else if (wat4 == 'Doffy'){
+				    	                                                                            	var finalblow = prompt('Wil je hem slaan of schoppen?', '"slaan" of "schoppen"')}
+				    	                                                                            	if (finalblow == 'schoppen'){
+				    	                                                                            	alert('Je wint!')}
+				    	                                                                            	else if (finalblow == 'slaan'){
+				    	                                                                            	alert('Doffy ontweek je slag.')}
+				    	                                                                                    if (finalblow == 'slaan' == true) var win = prompt('Wil je slaan of schoppen?', '"slaan" of "schoppen"')
+				    	                                                                                    if (win == 'slaan'){
+				    	                                                                                    alert('Je wint!')}
+				    	                                                                                    else if (win == 'schoppen'){
+				    	                                                                                    alert('Je wint!')}
+
+				    			
---
+++ kaas.js
@@ -0,0 +1,33 @@
+var geel = prompt('Is de kaas geel?') 
+    if (geel == 'ja'){
+    var gaten = prompt('Zitten er gaten in?')}
+	                                                        if (gaten == 'ja'){
+	                                                        var duur = prompt('Is de kaas belachelijk duur?')}
+	                                                                                                            if (duur == 'ja'){
+	                                                                                                            alert('Emmenthaler')}
+	                                                                                                            else if (duur == 'nee'){
+	                                                                                                            alert('Leerdammer')}
+	                                                        else if (gaten == 'nee'){
+	                                                        var hard = prompt('Is de kaas hard als steen?')}
+	                                                                                                            if (hard == 'ja'){
+	                                                                                                            alert('Parmigiano Reggiano')}
+	                                                                                                            else if (hard == 'nee'){
+	                                                                                                            alert('Goudse kaas')}
+    else if (geel == 'nee'){
+    var blauw = prompt('Heeft de kaas blauwe schimmels?')}
+                                                            if (blauw == 'ja'){
+                                                            var korst = prompt('Heeft de kaas een korts?')}   
+                                                                                                                if (korst == 'ja'){
+                                                                                                                alert('Blue de Rochbaron')}
+                                                                                                                else if (korst == 'nee'){
+                                                                                                                alert('Fourme')}	
+                                                            else if (blauw == 'nee'){
+                                                            var neekorst = prompt('Heeft de kaas een korst?')}
+                                                                                                                if (neekorst == 'ja'){
+                                                                                                                alert('Camembert')}
+                                                                                                                else if (neekorst == 'nee'){
+                                                                                                                alert('Mozzerella')}
+else{
+	alert('Typ ja of nee')
+}                                    
+                                                                        
