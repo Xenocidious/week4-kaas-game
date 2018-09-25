848c28db9dc8349a78a73e69206cfa33a893e2d5
Xeno <42929153+Xenophobiac@users.noreply.github.com>
2018-09-25 14:13:44 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,43 +1,100 @@
 alert('Jij en je bondgenoot Trafalgar komen 2 vijanden tegen, Doffy en Trebol. (Alles is hoofdlettergevoelig)')
-    var begin = prompt('Val jij of valt Trafalgar Doffy aan?', '"ik" of "Traf"')
-    if (begin == 'ik'){
-	alert('Doffy ontweek je aanval, Trebol heeft ondertussen Trafalgar vermoord.')}
-	    else if (begin == 'Traf'){
-	alert('Doffy vermoord Trafalgar en jij vermoord terwijl Trebol.')}
-	                                                                                if (begin == 'ik') var wie = prompt('Val je Doffy of Trebol aan?', '"Doffy" of "Trebol"')
-	                                                                                if (wie == 'Doffy'){
-	                                                                               	alert('Trebol vermoord je. (Game Over)')}
-	                                                                                else if (wie == 'Trebol'){
-		                                                                            alert('Je vermoord Trebol')}
-		                                                                                if (begin == 'Traf') var wat = prompt('Val je Doffy aan of ga je naar Gear 2? (Gears zijn powerups)', '"aanval" of "gear2"')
-		                                                                                var wat = prompt('Val je Doffy aan of ga je naar Gear 2? (Gears zijn powerups)', '"aanval" of "gear2"')
-		                                                                                if (wat == 'aanval'){
-		                                                                               	alert('Doffy blokkeert je aanval en vermoord je. (Game Over)')}
-		                                                                                else if (wat == 'gear2'){
-		                                                                               	alert('Je word sterker.')}
-		                                                                                    var wat2 = prompt('Val je Doffy aan of ga je naar Gear 3?', '"Doffy" of "gear3"')
-		                                                                                    if (wat2 == 'Doffy'){
-		                                                                                    alert('Doffy ontwijkt je aanval en vermoord je. (Game Over)	')}
-		                                                                                    else if (wat2 == "gear3"){
-		                                                                                    alert('Je word weer sterker.')}
-		                                                                                       	var wat3 = prompt('Val je Doffy aan of ga je naar Gear 4?', '"Doffy" of "gear4"')
-		                                                                                       	if (wat3 == 'Doffy'){
-		                                                                                       	alert('Doffy blokkeert je aanval en vermoord je. (Game Over)')}
-		                                                                                        else if (wat3 == 'gear4'){
-		                                                                                        alert('Je word nog sterker')}
-		                                                                                		    var wat4 = prompt('Val je Doffy aan of ga je naar Gear 5?', '"Doffy" of "gear5"')
-				    	                                                                            if (wat4 == 'gear5'){
-				    	                                                                           	alert('Je ontploft. (Game Over)')}
-				    	                                                                            else if (wat4 == 'Doffy'){
-				    	                                                                            	var finalblow = prompt('Wil je hem slaan of schoppen?', '"slaan" of "schoppen"')}
-				    	                                                                            	if (finalblow == 'schoppen'){
-				    	                                                                            	alert('Je wint!')}
-				    	                                                                            	else if (finalblow == 'slaan'){
-				    	                                                                            	alert('Doffy ontweek je slag.')}
-				    	                                                                                    if (finalblow == 'slaan' == true) var win = prompt('Wil je slaan of schoppen?', '"slaan" of "schoppen"')
-				    	                                                                                    if (win == 'slaan'){
-				    	                                                                                    alert('Je wint!')}
-				    	                                                                                    else if (win == 'schoppen'){
-				    	                                                                                    alert('Je wint!')}
+var input = prompt('Val jij of valt Trafalgar Doffy aan?', '"ik" of "Traf"');
 
-				    			
+if (input == 'ik'){
+    alert('Doffy ontweek je aanval, Trebol heeft ondertussen Trafalgar vermoord.')
+    input = prompt('Val je Doffy of Trebol aan?', '"Doffy" of "Trebol"');
+    if (input == 'Doffy'){
+    	alert('Trebol vermoord je. (Game Over)')
+    }
+    else{
+    	alert('Je vermoord Trebol')
+    	input = prompt('Val je Doffy aan of ga je naar gear2? (gears zijn powerups)', '"Doffy" of "gear2"');
+    	if (input == 'Doffy'){
+    		alert('Doffy blokkeert je aanval en vermoord je. (Game Over)')
+    	}
+    	else{
+    		alert('Je word sterker.')
+    		input = prompt('Val je Doffy aan of ga je naar gear3?', '"Doffy" of "gear3"');
+    		if (input == 'Doffy'){
+    			alert('Doffy ontwijkt je aanval en vermoord je. (Game Over)')
+    		} 
+    		else{
+    			alert('Je word weer sterker.')
+    			input = prompt('Val je Doffy aan of ga je naar gear4?', '"Doffy" of "gear4"');
+    			if (input == 'Doffy'){
+    				alert('Doffy blokkeert je aanval en vermoord je. (Game Over)')
+    			}
+    			else{
+    				alert('Je word nog sterker')
+    				input = prompt('Val je Doffy aan of ga je naar gear5?', '"Doffy" of "gear5"');
+    		    	if (input == 'gear5'){
+                      	alert('Je ontploft. (Game Over)')
+                    }
+    				else{
+    					input = prompt('Wil je hem slaan of schoppen?', '"slaan" of "schoppen"');
+    					if (input == 'schoppen'){
+                          	alert('Je wint!')
+                        }
+                        else{
+                        	alert('Doffy ontweek je slag.')
+				    	    input = prompt('Wil je slaan of schoppen?', '"slaan" of "schoppen"');
+				    	    if (input == 'slaan'){
+				    	        alert('Je wint!')
+				    	    }
+				    	    else{
+				    	        alert('Je wint!')
+				    	    }
+
+                        }
+    			    }
+    			}
+    		}	
+    	}
+    }
+}
+else{
+	alert('Doffy vermoord Trafalgar, en jij vermoord terwijl Trebol.')
+    input = prompt('Val je Doffy aan of ga je naar gear2? (gears zijn powerups)', '"Doffy" of "gear2"');
+    if (input == 'Doffy'){
+    		alert('Doffy blokkeert je aanval en vermoord je. (Game Over)')
+    }
+    else{
+    	alert('Je word sterker.')
+    	input = prompt('Val je Doffy aan of ga je naar gear3');
+    	if (input == 'Doffy'){
+    			alert('Doffy ontwijkt je aanval en vermoord je. (Game Over)')
+    	} 
+    	else{
+    		alert('Je word weer sterker.')
+    		input = prompt('Val je Doffy aan of ga je naar gear4?', '"Doffy" of "gear4"')
+    		if (input == 'Doffy'){
+    			alert('Doffy blokkeert je aanval en vermoord je. (Game Over)')
+    		}
+    		else{
+    			alert('Je word nog sterker')
+    			input = prompt('Val je Doffy aan of ga je naar gear5?', '"Doffy" of "gear5"')
+    		    if (input == 'gear5'){
+    		    	alert('Je ontploft. (Game Over)')
+    		    }
+    			else{
+    				input = prompt('Wil je hem slaan of schoppen?', '"slaan" of "schoppen"')
+    				if (input == 'schoppen'){
+                      	alert('Je wint!')
+                    }
+                    else{
+                      	alert('Doffy ontweek je slag.')
+				        input = prompt('Wil je slaan of schoppen?', '"slaan" of "schoppen"')
+				        if (input == 'slaan'){
+				            alert('Je wint!')
+				    	}
+				        else{
+				    	    alert('Je wint!')
+				        }
+
+                    }
+                }
+    	    }
+        }	
+    }
+}
